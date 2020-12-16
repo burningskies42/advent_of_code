543e17a65e2288821c7d5d090f8c21c59d855b0b
Leon <burningskies42@gmail.com>
2020-12-16 17:08:51 +0100

    added day6

---
+++ day6/puzzle1.py
@@ -0,0 +1,7 @@
+
+raw_data = open("input.txt", "r").read().split("\n\n")
+data_list = [[set(r1) for r1 in r.split("\n")] for r in raw_data]
+data_list = [(set.union(*r)) for r in data_list]
+data_len_list = [len(list(r)) for r in data_list]
+
+print(sum(data_len_list))
